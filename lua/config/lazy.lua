ec24777b0d905f882998a023ebf7777e5ea730aa
MarcoChavezB <marco1102004@gmail.com>
2024-06-03 10:20:51 -0600

    añadiendo metodos

---
+++ lua/config/lazy.lua
@@ -0,0 +1,71 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup({
+  spec = {
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "sonokai",
+        news = {
+          lazyvim = true,
+          neovim = true,
+        },
+      },
+    },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "plugins" },
+    {
+      "github/copilot.vim",
+      config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.api.nvim_set_keymap("i", "<C-P>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      end,
+    },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  dev = {
+    path = "~/.ghq/github.com",
+  },
+  checker = { enabled = true },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "rplugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+  debug = false,
+})
